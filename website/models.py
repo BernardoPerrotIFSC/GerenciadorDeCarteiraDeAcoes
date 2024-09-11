d8e7d52c93efb3c18051d29614237c754275901f
Bernardo Perrot <bernardo.sp@aluno.ifsc.edu.br>
2024-09-10 22:00:16 -0300

    Update models.py
    atualizaAcao, editAcao

--- website/models.py
+++ website/models.py
@@ -1,9 +1,11 @@
+from flask import flash
 from . import db
 from sqlalchemy.sql import func
 from flask_login import UserMixin
 from flask_admin.contrib.sqla import ModelView
 from . import admin
 from sqlalchemy import desc
+import yfinance as yf
 
 
 class Usuario(db.Model, UserMixin):
@@ -67,6 +69,51 @@ class Acao(db.Model):
     total_dividendos = db.Column(db.Float)
     retorno_dividendos = db.Column(db.Float)
 
+    def verAcao(self, usuario_id):
+        return Acao.query.filter_by(usuario_id=usuario_id).first()
+
+    def atualizaAcao(self, valor_total_careira):
+        ativo = yf.Ticker(self.ticker+".SA")
+        self.preco_atual = round(ativo.history(period='1d')['Close'].iloc[0], 2)
+        self.valor_atual = round(self.quantidade*self.preco, 2)
+        self.lucro_prejuizo = round(self.valor_atual-self.valor_pago, 2)
+        self.rentabilidade = round(self.lucro_prejuizo/self.valor_pago*100, 2)
+        if self.lucro_prejuizo == 0:
+            self.status = "zero"
+        elif self.lucro_prejuizo > 0:
+            self.status = "lucro"
+        else:
+            self.status = "prejuizo"
+        self.peso = round((self.valor_atual/valor_total_careira)*100)
+        db.session.commit()
+
+    def editAcao(self, preco_medio, quantidade):
+        self.preco_medio = preco_medio
+        self.quantidade = quantidade
+        db.session.commit()
+        
+    def removeAcaoCriaHist(self, preco_venda, quantidade, descricao, data_venda):
+        if preco_venda <=0:
+            flash("Preço ou quantidade não foram encontradas", category='error')
+        elif quantidade > self.quantidade:
+            flash("Ação ou quantidade não foram encontradas", category='error')
+        elif quantidade <self.quantidade:
+            return False
+            #Funcao deve atualizar os dividendos primeiro
+        elif quantidade == self.quantidade:
+            #Funcao deve atualizar os dividendos primeiro
+            return False
+            
+
+class CompraAcao(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    ticker = db.Column(db.String(10))
+    preco_pago = db.Column(db.Float)
+    quantidade = db.Column(db.Integer)
+    valor_pago = db.Column(db.Float)
+    descricao = db.String(db.String)
+    data_compra = db.Column(db.Date)
     
 
 
