4d427ca7757ec2fb9d8e4a82463b04852440191c
Bernardo Perrot <bernardo.sp@aluno.ifsc.edu.br>
2024-09-13 00:20:28 -0300

    Classes
    Criacao de classes HistOperacoes, HistDividendos e Hist. Funcao do Usuario addAcao.

--- website/models.py
+++ website/models.py
@@ -1,4 +1,4 @@
-from flask import flash
+from flask import flash, redirect, url_for
 from . import db
 from sqlalchemy.sql import func
 from flask_login import UserMixin
@@ -6,6 +6,7 @@ from flask_admin.contrib.sqla import ModelView
 from . import admin
 from sqlalchemy import desc
 import yfinance as yf
+import datetime
 
 
 class Usuario(db.Model, UserMixin):
@@ -13,11 +14,67 @@ class Usuario(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     senha = db.Column(db.String(150))
     nome = db.Column(db.String(150))
+    compra_acao = db.relationship('CompraAcao')
+    acao = db.relationship('Acao')
+    carteira = db.relationship('Carteira')
 
     def VerUsuario(usuario_id):
-        return Usuario.query.filter_by(usuario_id=usuario_id).first()
+        return Usuario.query.filter_by(id=usuario_id).first()
+    
+    def addAcao(self, ticker, preco_pago, quantidade, descricao, data_compra_str):
+        ticker = ticker.upper()
+        acao_query = Acao.query.filter_by(ticker=ticker).filter_by(usuario_id=self.id).first()
+        if acao_query:
+            ativo = yf.Ticker(ticker+".SA")
+            preco_atual = round(ativo.history(period='1d')['Close'].iloc[0], 2)
+            valor_pago = round(preco_pago*quantidade, 2)
+            acao_query.quantidade = acao_query.quantidade + quantidade
+            acao_query.preco_medio = round((acao_query.valor_pago+valor_pago)/(acao.quantidade), 2)
+            acao_query.valor_pago = round(acao_query.valor_pago + valor_pago, 2)
+            acao_query.valor_atual = round(preco_atual*acao_query.quantidade, 2)
+            acao_query.lucro_prejuizo = round(acao_query.valor_atual - acao.valor_pago, 2)
+            acao_query.rentabilidade = round(acao_query.lucro_prejuizo/acao.valor_pago*100, 2)
+            if acao_query.rentabilidade > 0:
+                acao_query.status = "lucro"
+            elif acao_query.rentabilidade == 0:
+                acao_query.status = "zero"
+            else:
+                acao_query.status = "prejuizo"
+            db.session.commit()
 
-class Carteira(db.model):
+            data_compra = datetime.strptime(data_compra_str, '%Y-%m-%d')
+            nova_compra = CompraAcao(ticker = ticker, preco_pago = preco_pago, quantidade = quantidade, valor_pago = valor_pago, data_compra = data_compra, usuario_id = self.id)
+            hist_acao = Historico(usuario_id = self.id, ticker=ticker, descricao=descricao, quantidade=quantidade, preco = preco_pago, valor = valor_pago, tipo = "compra", data = data_compra)
+            db.session.add(nova_compra)
+            db.session.add(hist_acao)
+            db.session.commit()
+            flash(f"Mais {quantidade} acoes foram adicionadas a {acao.ticker}")
+            return redirect(url_for('views.add_acao'))
+        else:
+            valor_pago = round(preco_pago*quantidade, 2)
+            acao = yf.Ticker(ticker+".SA")
+            preco_atual = round(acao.history(period='1d')['Close'].iloc[0], 2)
+            valor_atual = round(preco_atual*quantidade, 2)
+            lucro_prejuizo = round(valor_atual-valor_pago, 2)
+            rentabilidade = round(lucro_prejuizo/valor_pago*100, 2)
+            data_compra = datetime.strptime(data_compra_str, '%Y-%m-%d')
+            if rentabilidade > 0:
+                status = "lucro"
+            elif rentabilidade == 0:
+                status = "zero"
+            else:
+                status = "prejuizo"
+            nova_compra = CompraAcao(ticker = ticker, preco_pago = preco_pago, quantidade = quantidade, valor_pago = valor_pago, data_compra = data_compra, usuario_id = self.id)
+            acao = Acao(ticker = ticker, preco_medio = preco_pago, quantidade = quantidade, valor_pago = valor_pago, preco_atual = preco_atual, valor_atual= valor_atual,rentabilidade = rentabilidade, lucro_prejuizo=lucro_prejuizo, status = status, usuario_id = self.id)
+            hist_acao = Historico(usuario_id = self.id, ticker=ticker, descricao=descricao, quantidade=quantidade, preco_pago = preco_pago, valor_pago = valor_pago, tipo = "compra", data = data_compra)
+            db.session.add(hist_acao)
+            db.session.add(acao)
+            db.session.add(nova_compra)
+            db.session.commit()
+            flash(f"{ticker} adicionada a carteira", category='success')
+            return redirect(url_for('views.add_acao'))
+
+class Carteira(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
     valor_pago = db.Column(db.Float, default=0.0)
@@ -71,6 +128,9 @@ class Acao(db.Model):
 
     def verAcao(self, usuario_id):
         return Acao.query.filter_by(usuario_id=usuario_id).first()
+    
+    def verAcoes(self, usuario_id):
+        return Acao.query.filter_by(usuario_id=usuario_id)
 
     def atualizaAcao(self, valor_total_careira):
         ativo = yf.Ticker(self.ticker+".SA")
@@ -104,7 +164,6 @@ class Acao(db.Model):
             #Funcao deve atualizar os dividendos primeiro
             return False
             
-
 class CompraAcao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
@@ -139,6 +198,39 @@ class CompraAcao(db.Model):
         #     # Acumula o total de dividendos
         #     total = round(total + dividend, 4)
 
+class Historico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    ticker = db.Column(db.String(10))
+    preco_pago = db.Column(db.Float)
+    quantidade = db.Column(db.Integer)
+    valor_pago = db.Column(db.Float)
+    descricao = db.Column(db.String)
+    tipo = db.Column(db.String)
+    data = db.Column(db.Date)
+
+class HistOperacoes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    ticker = db.Column(db.String(10))
+    preco_compra = db.Column(db.Float)
+    quantidade_compra = db.Column(db.Integer)
+    valor_compra = db.Column(db.Float)
+    preco_venda = db.Column(db.Float)
+    quantidade_venda = db.Column(db.Integer)
+    lucro_prejuizo = db.Column(db.Float)
+    rentabilidade = db.Column(db.Float)
+    
+class HistDividendos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    ticker = db.Column(db.String(10))
+    quantidade = db.Column(db.Integer)
+    valor_por_acao = db.Column(db.Float)
+    valor = db.Column(db.Float)
+    data = db.Column(db.Date)
+
+    
 
 
 
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .models import *
 
 views = Blueprint('views',__name__)
 
@@ -8,3 +9,9 @@ views = Blueprint('views',__name__)
 @login_required
 def home():
     return render_template("home.html", usuario=current_user)
+
+@views.route('/carteira', methods=["GET","POST"])
+def carteira():
+    acoes = Acao.verAcoes(current_user)
+    carteira = Carteira.VerCarteira(current_user)
+    return render_template("carteira.html", usuario=current_user, acoes=acoes, carteira=carteira)
